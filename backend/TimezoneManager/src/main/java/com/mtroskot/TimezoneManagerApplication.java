b9ef248b5a90ddb531d1dd4f76f0b04efe782cbc
Marko Troskot <marko.troskot@hotmail.com>
2020-03-21 20:10:45 +0100

    Added SpringBoot app

---
+++ backend/TimezoneManager/src/main/java/com/mtroskot/TimezoneManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mtroskot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezoneManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimezoneManagerApplication.class, args);
+	}
+
+}
---
+++ backend/TimezoneManager/src/test/java/com/mtroskot/TimezoneManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtroskot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezoneManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
