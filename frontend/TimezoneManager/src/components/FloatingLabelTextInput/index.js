c50326101c384698d7dfa121432ae6c606ab058d
Marko Troskot <mtroskot@rccl.com>
2020-03-24 12:53:06 +0100

    updated FloatingLabel component

--- frontend/TimezoneManager/src/components/FloatingLabelTextInput/index.js
+++ frontend/TimezoneManager/src/components/FloatingLabelTextInput/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Animated, TextInput, View } from 'react-native';
+import { Animated, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import styles from 'src/components/FloatingLabelTextInput/styles';
 import StringUtils from 'src/utils/StringUtils';
 import { dimensions } from 'src/styles';
 import HooksUtils from 'src/utils/HooksUtils';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { AppUtils } from 'src/utils';
 
 const { rem } = dimensions;
 
@@ -12,9 +14,12 @@ const FloatingLabelTextInput = ({
   onSubmitEditing,
   textInputRef,
   floatingLabel,
+  label,
   value,
+  error,
   textInputStyle,
   customContainerStyle,
+  iconProps,
   ...restProps
 }) => {
   const [floatAnim] = useState(new Animated.Value(0));
@@ -23,7 +28,7 @@ const FloatingLabelTextInput = ({
   HooksUtils.useDidUpdate(() => {
     if (StringUtils.isEmpty(value)) {
       Animated.timing(floatAnim, {
-        toValue: focused ? -25 * rem : 0 * rem,
+        toValue: focused ? -25 * rem : 0,
         duration: 250,
         useNativeDriver: true
       }).start();
@@ -39,39 +44,73 @@ const FloatingLabelTextInput = ({
   };
 
   return (
-    <View style={[styles.container, customContainerStyle]}>
-      <View style={styles.subContainer}>
-        {floatingLabel && (
-          <Animated.Text
-            style={{
-              ...styles.label,
-              transform: [{ translateY: floatAnim }]
-            }}>
-            {floatingLabel}
-          </Animated.Text>
-        )}
-        <TextInput
-          value={value}
-          underlineColorAndroid="transparent"
-          ref={textInputRef}
-          onSubmitEditing={onSubmitEditing}
-          style={[styles.defaultInput, textInputStyle]}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          {...restProps}
-        />
+    <View style={[styles.outerContainer, customContainerStyle]}>
+      <View style={[styles.container, error ? styles.errorContainer : null]}>
+        <View style={styles.inlineView}>
+          <View style={styles.subContainer}>
+            {floatingLabel && (
+              <Animated.Text
+                style={{
+                  ...styles.label,
+                  transform: [{ translateY: floatAnim }]
+                }}>
+                {floatingLabel}
+              </Animated.Text>
+            )}
+            {label && <Text style={styles.fixedLabel}>{label}</Text>}
+            <TextInput
+              value={value}
+              underlineColorAndroid="transparent"
+              ref={textInputRef}
+              onSubmitEditing={onSubmitEditing}
+              style={[styles.defaultInput, textInputStyle]}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              {...restProps}
+            />
+          </View>
+          {iconProps && (
+            <TouchableOpacity
+              onPress={iconProps.onIconPress}
+              disabled={!iconProps.onIconPress}
+              style={styles.iconContainer}
+              hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}>
+              <Icon
+                name={AppUtils.getIconForPlatform(iconProps.name, iconProps.prefix)}
+                size={iconProps.size || 30 * rem}
+                color={iconProps.color || '#fff'}
+              />
+            </TouchableOpacity>
+          )}
+        </View>
       </View>
+      {error && <Text style={styles.errorText}>{floatingLabel || label} is required</Text>}
     </View>
   );
 };
 
 FloatingLabelTextInput.propTypes = {
+  // eslint-disable-next-line react/no-unused-prop-types
+  floatingLabelOrLabel: function(props, propName, componentName) {
+    const { floatingLabel, label } = props;
+    if (!floatingLabel && !label) {
+      return new Error(`FloatingLabel or Label is required in ${componentName}`);
+    }
+  },
   onSubmitEditing: PropTypes.func,
   textInputRef: PropTypes.shape({ current: PropTypes.any }),
-  floatingLabel: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  floatingLabel: PropTypes.string,
+  label: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.bool,
   textInputStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-  customContainerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
+  customContainerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  iconProps: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    onIconPress: PropTypes.func,
+    size: PropTypes.number,
+    color: PropTypes.string
+  })
 };
 
 export default React.memo(FloatingLabelTextInput);
--- frontend/TimezoneManager/src/components/FloatingLabelTextInput/styles.js
+++ frontend/TimezoneManager/src/components/FloatingLabelTextInput/styles.js
@@ -4,15 +4,31 @@ const { rem } = dimensions;
 const paddingLeft = 15 * rem;
 
 const styles = StyleSheet.create({
-  container: {
+  outerContainer: {
     width: '90%',
+    alignSelf: 'center'
+  },
+  container: {
     borderColor: '#DFE5EA',
     borderWidth: 1 * rem,
-    alignSelf: 'center',
     borderRadius: 5 * rem,
     marginBottom: 10 * rem,
     height: 60 * rem,
     backgroundColor: '#F5F6F7'
+  },
+  errorContainer: {
+    borderColor: 'red',
+    marginBottom: 0
+  },
+  errorText: {
+    ...fonts.ubuntu.normal.light,
+    fontSize: 10 * rem,
+    marginBottom: 10 * rem,
+    color: 'red'
+  },
+  inlineView: {
+    flexDirection: 'row',
+    flex: 1
   },
   subContainer: {
     flex: 1,
@@ -21,16 +37,29 @@ const styles = StyleSheet.create({
     marginBottom: 10 * rem
   },
   label: {
+    ...fonts.ubuntu.normal.light,
     position: 'absolute',
     left: paddingLeft,
     color: '#949EA0',
     fontSize: 13 * rem
+  },
+  fixedLabel: {
+    ...fonts.ubuntu.normal.light,
+    position: 'absolute',
+    left: paddingLeft,
+    color: '#949EA0',
+    fontSize: 13 * rem,
+    bottom: 25 * rem
   },
   defaultInput: {
     ...fonts.ubuntu.normal.regular,
     fontSize: 16 * rem,
     color: '#334144',
     padding: 0
+  },
+  iconContainer: {
+    justifyContent: 'center',
+    marginRight: 15
   }
 });
 
