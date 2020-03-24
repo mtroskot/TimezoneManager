dee63c838ff005972240b9654b7409f82e257d0e
Marko Troskot <mtroskot@rccl.com>
2020-03-24 22:30:00 +0100

    added SearchBar component

---
+++ frontend/TimezoneManager/src/components/SearchBar/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { TextInput, View, ViewPropTypes } from 'react-native';
+import { CustomButton } from 'src/components';
+import PropTypes from 'prop-types';
+import { icons } from 'src/constants/icons';
+import styles from 'src/components/SearchBar/styles';
+import StringUtils from 'src/utils/StringUtils';
+import { dimensions } from 'src/styles';
+const { rem } = dimensions;
+
+const SearchBar = props => {
+  const { searchInput, handleInput, placeholder, viewStyle, clearInput } = props;
+  const searchInputEmpty = StringUtils.isEmpty(searchInput);
+  return (
+    <View style={[styles.textInputView, viewStyle]}>
+      <TextInput
+        style={styles.textInputStyle}
+        value={searchInput}
+        placeholder={placeholder}
+        placeholderTextColor="#949EA0"
+        onChangeText={handleInput}
+      />
+      <CustomButton
+        iconStyle={styles.iconStyle}
+        iconProps={{
+          name: searchInputEmpty ? icons.SEARCH : icons.CLOSE_CIRCLE,
+          color: searchInputEmpty ? '#959595' : '#404040',
+          size: 20 * rem
+        }}
+        onPress={clearInput}
+        disabled={searchInputEmpty}
+      />
+    </View>
+  );
+};
+
+SearchBar.propTypes = {
+  searchInput: PropTypes.string.isRequired,
+  handleInput: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  viewStyle: ViewPropTypes.style,
+  clearInput: PropTypes.func.isRequired
+};
+
+SearchBar.defaultProps = {
+  placeholder: 'Search'
+};
+
+export default React.memo(SearchBar);
